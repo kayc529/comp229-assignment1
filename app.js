fea4fad25ae890db57845f9f6e2e70dfc71de248
Kay <kaicipher966@gmail.com>
2022-06-01 15:53:07 -0400

    added home, about and projects page

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
 
 let app = express();
 
@@ -22,22 +21,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', { title: 'Error'});
+  res.render('error', { title: 'Error' });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+router.get('/', function (req, res, next) {
+  res.render('index', { page: 'home' });
 });
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+router.get('/home', function (req, res, next) {
+  res.render('index', { page: 'home' });
 });
 
-/* GET About Us page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+/* GET About page. */
+router.get('/about', function (req, res, next) {
+  res.render('index', { page: 'about' });
 });
 
-/* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products'});
+/* GET Projects page. */
+router.get('/projects', function (req, res, next) {
+  res.render('index', { page: 'projects' });
 });
 
 /* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+router.get('/services', function (req, res, next) {
+  res.render('index', { page: 'services' });
 });
 
 /* GET Contact Us page. */
-router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+router.get('/contact', function (req, res, next) {
+  res.render('index', { page: 'contact' });
 });
 
 module.exports = router;
