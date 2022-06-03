d3b98e26792a90d73778a1c0af1f23535824db85
Kay <kaicipher966@gmail.com>
2022-06-03 13:19:47 -0400

    added services and contact pages

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,11 +1,33 @@
+/* File name: app.js */
+/* Student's name: Kay Yan Cheung */
+/* StudentID: 301183574 */
+/* Date: Jun 3 2022 */
+
 // IIFE -- Immediately Invoked Function Expression
-(function(){
+(function () {
+  function Start() {
+    console.log('App Started...');
+  }
 
-    function Start()
-    {
-        console.log("App Started...");
-    }
+  window.addEventListener('load', Start);
 
-    window.addEventListener("load", Start);
+  let id = document.getElementById('submit-btn');
 
+  //submit contact form
+  if (id) {
+    id.onclick = function (e) {
+      e.preventDefault();
+      let name = document.getElementById('contact-name').value;
+      let email = document.getElementById('contact-email').value;
+      let content = document.getElementById('contact-content').value;
+
+      if (!name || !email || !content) {
+        alert('Please fill in the form!');
+        return;
+      }
+
+      alert(`Thank you ${name}! Your form is submitted!`);
+      window.location.href = '/';
+    };
+  }
 })();
